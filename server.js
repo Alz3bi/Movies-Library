4a597aad4359ae434aea89cb3ffc92cc2e9bd8ae
Alz3bi <mohammed.alzoubi.dev@gmail.com>
2024-05-05 14:31:11 +0000

    feat: PUT and DELETE

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const moviesData = require("./Movie Data/data.json")
-const axios = require("axios")
+const axios = require("axios").default
 const cors = require("cors")
 const pg = require("pg")
 require("dotenv").config();
@@ -8,6 +8,7 @@ require("dotenv").config();
 const server = express();
 const PORT = 3000;
 const client =new pg.Client("postgressql://localhost:5432/movieslibrary")
+let apiKey = process.env.apiKey;
 
 function Movies(id, title, release_date, poster_path, overview) {
     this.id = id;
@@ -29,12 +30,15 @@ server.use(express.json())
 
 server.get("/", homePageHandler)
 server.get("/favorite", favoritePageHandler)
-server.get("/search", searchHandler)
+server.get("/search/:title", searchHandler)
 server.get("/trendingMovies", trendingMoviesHandler)
 server.get("/popularMovies", popularMoviesHandler)
 server.get("/trendingTVs", trendingTVsHandler)
 server.post("/addMovie", addMovieHandler)
+server.put("/updateMovie/:id", updateMovieHandler)
+server.delete("/deleteMovie/:id", deleteMovieHandler)
 server.get("/getMovies",getMoviesHandler)
+server.get("/getMovie/:id",getMovieHandler)
 server.get("*", clientErrorHnadler)
 server.use(serverErrorHandler)
 
@@ -65,8 +69,7 @@ function favoritePageHandler(req, res){
 
 function searchHandler(req, res) {
     try{
-        const search = "spider"
-        let apiKey = process.env.apiKey;
+        const search = req.params.title
         let url = `https://api.themoviedb.org/3/search/movie?query=${search}&api_key=${apiKey}`
         axios.get(url)
             .then(axiosResult => {
@@ -87,9 +90,7 @@ function searchHandler(req, res) {
 
 function trendingMoviesHandler(req, res) {
     try{
-        let apiKey = process.env.apiKey;
         let url = `https://api.themoviedb.org/3/trending/movie/day?api_key=${apiKey}`
-
         axios.get(url)
             .then(axiosResult => {
                 let mapResult = axiosResult.data.results.map(movie => {
@@ -109,9 +110,7 @@ function trendingMoviesHandler(req, res) {
 
 function popularMoviesHandler(req, res) {
     try{
-        let apiKey = process.env.apiKey;
         let url = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`
-
         axios.get(url)
             .then(axiosResult => {
                 let mapResult = axiosResult.data.results.map(movie => {
@@ -131,9 +130,7 @@ function popularMoviesHandler(req, res) {
 
 function trendingTVsHandler(req, res) {
     try{
-        let apiKey = process.env.apiKey;
         let url = `https://api.themoviedb.org/3/trending/tv/day?api_key=${apiKey}`
-
         axios.get(url)
             .then(axiosResult => {
                 let mapResult = axiosResult.data.results.map(show => {
@@ -152,12 +149,48 @@ function trendingTVsHandler(req, res) {
 }
 
 function addMovieHandler(req, res) {
-    const movie = req.body 
-    const sql = "INSERT INTO movies(movie_title, movie_release_date, movie_poster_path, movie_overview) VALUES($1, $2, $3, $4) RETURNING *;"
-    const values = [movie.title, movie.release_date, movie.poster_path, movie.overview]
-    client.query(sql, values)
-        .then((data) => {
-            res.send("your data was added")
+    try{
+        const movie = req.body 
+        const sql = "INSERT INTO movies(movie_title, movie_release_date, movie_poster_path, movie_overview) VALUES($1, $2, $3, $4) RETURNING *;"
+        const values = [movie.title, movie.release_date, movie.poster_path, movie.overview]
+        client.query(sql, values)
+            .then((data) => {
+                res.send("your data was added")
+            })
+            .catch((error) =>{
+                serverErrorHandler(error, req, res)
+            })
+    }
+    catch(error){
+        serverErrorHandler(error, req, res)
+    }
+}
+
+function updateMovieHandler(req, res) {
+    try{
+        const id = req.params.id
+        const sql = `UPDATE movies SET movie_title=$1, movie_release_date=$2 ,movie_poster_path=$3 ,movie_overview=$4 WHERE movie_id = ${id} RETURNING *`
+        const values = [req.body.title, req.body.release_date, req.body.poster_path, req.body.overview]
+        console.log(values);
+        client.query(sql, values)
+            .then((data) => {
+                res.json(data.rows)
+            })
+            .catch((error) =>{
+                serverErrorHandler(error, req, res)
+            })
+    }
+    catch(error){
+        serverErrorHandler(error, req, res)
+    }
+}
+
+function deleteMovieHandler(req, res) {
+    const id = req.params.id
+    const sql = `DELETE FROM movies WHERE movie_id = ${id}`
+    client.query(sql)
+        .then((data) =>{
+            res.status(204).json({})
         })
         .catch((error) =>{
             serverErrorHandler(error, req, res)
@@ -167,6 +200,22 @@ function addMovieHandler(req, res) {
 function getMoviesHandler(req, res) {
     try{
         const sql = "SELECT * FROM movies;"
+        client.query(sql)
+        .then((data) => {
+            res.send(data.rows)
+        })
+        .catch((error) => {
+            serverErrorHandler(error, req, res)
+        })
+    }
+    catch(error){
+        serverErrorHandler(error, req, res)
+    }
+}
+function getMovieHandler(req, res) {
+    try{
+        const id = req.params.id
+        const sql = `SELECT * FROM movies WHERE movie_id = ${id}`
         client.query(sql)
         .then((data) => {
             res.send(data.rows)
